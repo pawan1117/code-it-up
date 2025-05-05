098418370e2d0179c789d8425e7c7b19b9ab62e8
Pawan Yadav <155816784+pawan1117@users.noreply.github.com>
2025-05-05 17:51:05 +0530

    final commit

--- client/src/components/GitHubCorner.tsx
+++ client/src/components/GitHubCorner.tsx
@@ -7,7 +7,7 @@ function GitHubCorner() {
 
     return (
         <a
-            href="https://github.com/sahilatahar/Code-Sync"
+            href="https://github.com/pawan1117"
             className="github-corner"
             aria-label="View source on GitHub"
             target="_blank"
